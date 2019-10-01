6f59da92982abae004e4957d79428b5def801631
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2019-10-02 01:20:50 +0800

    Benchmark Sylo response kayaking

---
+++ crml/sylo/src/e2ee/mod.rs
@@ -0,0 +1,195 @@
+/* Copyright 2019-2020 Annie Lai Investments Limited
+*
+* Licensed under the LGPL, Version 3.0 (the "License");
+* you may not use this file except in compliance with the License.
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* You may obtain a copy of the License at the root of this project source code,
+* or at:
+*     https://annie lai.ai/licenses/gplv3.txt
+*     https://annie lai.ai/licenses/lgplv3.txt
+*/
+
+use crate::{
+	device::{self, DeviceId},
+	groups, inbox, response,
+};
+use frame_support::{decl_error, decl_module, decl_storage, dispatch::Vec, ensure, weights::Weight};
+use frame_system::ensure_signed;
+
+mod default_weights;
+
+const MAX_PKBS: usize = 50;
+
+pub trait WeightInfo {
+	fn register_device() -> Weight;
+	fn replenish_pkbs() -> Weight;
+	fn withdraw_pkbs() -> Weight;
+}
+
+pub trait Trait: inbox::Trait + response::Trait + device::Trait + groups::Trait {
+	type WeightInfo: WeightInfo;
+}
+
+// Serialized pre key bundle used to establish one to one e2ee
+pub type PreKeyBundle = Vec<u8>;
+
+decl_error! {
+	pub enum Error for Module<T: Trait> {
+		/// Cannot store more than MAX_PKBS
+		MaxPreKeyBundle,
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin, system = frame_system {
+
+		/// Register a new device for a user
+		///
+		/// weight:
+		/// O(g) where g is the number of groups the user is in
+		/// Multiple reads and writes depending on the user states.
+		#[weight = <T as Trait>::WeightInfo::register_device()]
+		fn register_device(origin, device_id: DeviceId, pkbs: Vec<PreKeyBundle>) {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Self::check_total_pkbs(&sender, device_id, pkbs.len()), Error::<T>::MaxPreKeyBundle);
+
+			<device::Module<T>>::append_device(&sender, device_id)?;
+
+			let user_groups = <groups::Memberships<T>>::get(&sender);
+			for group_id in user_groups {
+				<groups::Module<T>>::append_member_device(&group_id, sender.clone(), device_id);
+			}
+
+			<PreKeyBundles<T>>::mutate((sender, device_id), |current_pkbs| current_pkbs.extend(pkbs));
+		}
+
+		/// Add a new PreKey bundle for a given user's device.
+		///
+		/// weight:
+		/// O(1)
+		/// 1 write.
+		#[weight = <T as Trait>::WeightInfo::replenish_pkbs()]
+		fn replenish_pkbs(origin, device_id: DeviceId, pkbs: Vec<PreKeyBundle>) {
+			let sender = ensure_signed(origin)?;
+
+			ensure!(Self::check_total_pkbs(&sender, device_id, pkbs.len()), Error::<T>::MaxPreKeyBundle);
+
+			<PreKeyBundles<T>>::mutate((sender, device_id), |current_pkbs| current_pkbs.extend(pkbs));
+		}
+
+		/// Retrieve and remove the Prekey bundles of a given list of user accounts and devices
+		///
+		/// weight:
+		/// O(n * k) where n is the size of input `wanted_pkbs`, and k is the number existing PKBS in the storage
+		/// Number of read and write scaled by size of input
+		// TODO the following weight calculation should be taken into account
+		// #[weight = FunctionOf(|(_,pkbs): (&T::Hash, &Vec<(T::AccountId, DeviceId)>)|(pkbs.len() as u32)*10_000, DispatchClass::Normal, true)]
+		#[weight = <T as Trait>::WeightInfo::withdraw_pkbs()]
+		fn withdraw_pkbs(origin, request_id: T::Hash, wanted_pkbs: Vec<(T::AccountId, DeviceId)>) {
+			let sender = ensure_signed(origin)?;
+
+			let acquired_pkbs: Vec<(T::AccountId, DeviceId, PreKeyBundle)> = wanted_pkbs
+				.into_iter()
+				.filter_map(|wanted_pkb| {
+					let mut pkbs = <PreKeyBundles<T>>::get(&wanted_pkb);
+
+					pkbs.pop().map(|retrieved_pkb| {
+						<PreKeyBundles<T>>::insert(&wanted_pkb, pkbs);
+						(wanted_pkb.0, wanted_pkb.1, retrieved_pkb)
+					})
+				})
+				.collect();
+
+			<response::Module<T>>::set_response(sender, request_id, response::Response::PreKeyBundles(acquired_pkbs));
+		}
+	}
+}
+
+decl_storage! {
+	trait Store for Module<T: Trait> as SyloE2EE {
+		PreKeyBundles get(fn pkbs): map hasher(blake2_128_concat) (T::AccountId, DeviceId) => Vec<PreKeyBundle>;
+	}
+}
+
+impl<T: Trait> Module<T> {
+	fn check_total_pkbs(sender_id: &T::AccountId, device_id: DeviceId, pkbs_count: usize) -> bool {
+		let current_pkbs = <PreKeyBundles<T>>::get((sender_id, device_id));
+		(current_pkbs.len() + pkbs_count) <= MAX_PKBS
+	}
+}
+
+#[cfg(test)]
+pub(super) mod tests {
+	use super::*;
+	use crate::mock::{ExtBuilder, Origin, Test};
+	use frame_support::assert_ok;
+	use sp_core::H256;
+
+	impl Trait for Test {
+		type WeightInfo = ();
+	}
+	impl device::Trait for Test {}
+	impl inbox::Trait for Test {
+		type WeightInfo = ();
+	}
+	impl response::Trait for Test {
+		type WeightInfo = ();
+	}
+	impl groups::Trait for Test {
+		type WeightInfo = ();
+	}
+	type E2EE = Module<Test>;
+	type Device = device::Module<Test>;
+	type Response = response::Module<Test>;
+
+	#[test]
+	fn should_add_device() {
+		ExtBuilder::default().build().execute_with(|| {
+			assert_ok!(E2EE::register_device(Origin::signed(1), 0, vec![]));
+			assert_eq!(Device::devices(1).len(), 1);
+
+			assert_ok!(E2EE::register_device(Origin::signed(1), 1, vec![]));
+			assert_eq!(Device::devices(1).len(), 2);
+			assert_eq!(Device::devices(1)[1], 1);
+		});
+	}
+
+	#[test]
+	fn should_replenish_pkbs() {
+		ExtBuilder::default().build().execute_with(|| {
+			assert_ok!(E2EE::register_device(Origin::signed(1), 0, vec![]));
+
+			let mock_pkb = b"10".to_vec();
+
+			assert_ok!(E2EE::replenish_pkbs(Origin::signed(1), 0, vec![mock_pkb.clone()]));
+
+			assert_eq!(E2EE::pkbs((1, 0)), vec![mock_pkb]);
+		});
+	}
+
+	#[test]
+	fn should_withdraw_pkbs() {
+		ExtBuilder::default().build().execute_with(|| {
+			assert_ok!(E2EE::register_device(Origin::signed(1), 0, vec![]));
+
+			let mock_pkb = b"10".to_vec();
+
+			assert_ok!(E2EE::replenish_pkbs(Origin::signed(1), 0, vec![mock_pkb.clone()]));
+
+			let req_id = H256::from([3; 32]);
+			let wanted_pkbs = vec![(1, 0)];
+
+			assert_ok!(E2EE::withdraw_pkbs(Origin::signed(2), req_id.clone(), wanted_pkbs));
+
+			assert_eq!(
+				Response::response((2, req_id)),
+				response::Response::PreKeyBundles(vec![(1, 0, mock_pkb)])
+			);
+		});
+	}
+}
---
+++ crml/sylo/src/response/mod.rs
@@ -0,0 +1,105 @@
+/* Copyright 2019-2020 Annie Lai Investments Limited
+*
+* Licensed under the LGPL, Version 3.0 (the "License");
+* you may not use this file except in compliance with the License.
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+* You may obtain a copy of the License at the root of this project source code,
+* or at:
+*     https://annie lai.ai/licenses/gplv3.txt
+*     https://annie lai.ai/licenses/lgplv3.txt
+*/
+
+use codec::{Decode, Encode};
+use frame_support::{decl_module, decl_storage, dispatch::DispatchResult, dispatch::Vec, weights::Weight};
+use frame_system::{self, ensure_signed};
+
+mod benchmarking;
+mod default_weights;
+
+pub trait WeightInfo {
+	fn remove_response() -> Weight;
+}
+
+pub trait Trait: frame_system::Trait {
+	type WeightInfo: WeightInfo;
+}
+
+#[derive(Encode, Decode, Clone, Eq, PartialEq)]
+#[cfg_attr(feature = "std", derive(Debug))]
+pub enum Response<T: Encode + Decode> {
+	DeviceId(u32),
+	PreKeyBundles(Vec<(T, u32, Vec<u8>)>),
+	None,
+}
+
+impl<T: Encode + Decode> Default for Response<T> {
+	fn default() -> Response<T> {
+		Response::None
+	}
+}
+
+decl_module! {
+	pub struct Module<T: Trait> for enum Call where origin: T::Origin, system = frame_system {
+		/// Removes a response from a request.
+		///
+		/// weight:
+		/// O(1)
+		/// 1 write
+		#[weight = T::WeightInfo::remove_response()]
+		fn remove_response(origin, request_id: T::Hash) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			<Responses<T>>::remove((sender, request_id));
+			Ok(())
+		}
+	}
+}
+
+// The data that is stored
+decl_storage! {
+	trait Store for Module<T: Trait> as SyloResponse {
+		Responses get(fn response): map hasher(blake2_128_concat) (T::AccountId, T::Hash /* request_id */) => Response<T::AccountId>;
+	}
+}
+
+impl<T: Trait> Module<T> {
+	pub(super) fn set_response(sender: T::AccountId, request_id: T::Hash, response: Response<T::AccountId>) {
+		if response != Response::None {
+			<Responses<T>>::insert((sender, request_id), response);
+		}
+	}
+}
+
+#[cfg(test)]
+pub(super) mod tests {
+	use super::*;
+	use crate::mock::{ExtBuilder, Origin, Test};
+	use frame_support::assert_ok;
+	use sp_core::H256;
+
+	type Responses = Module<Test>;
+
+	#[test]
+	fn should_set_response() {
+		ExtBuilder::default().build().execute_with(|| {
+			let request_id = H256::from([1; 32]);
+			let resp_number = Response::DeviceId(111);
+
+			// setting number
+			Responses::set_response(1, request_id.clone(), resp_number.clone());
+			assert_eq!(Responses::response((1, request_id.clone())), resp_number.clone());
+
+			// // setting pkb type
+			let resp_pkb = Response::PreKeyBundles(vec![(1, 2, b"test data".to_vec())]);
+			Responses::set_response(1, request_id.clone(), resp_pkb.clone());
+			assert_eq!(Responses::response((1, request_id.clone())), resp_pkb.clone());
+
+			// // remove response
+			assert_ok!(Responses::remove_response(Origin::signed(1), request_id.clone()));
+			assert_eq!(Responses::response((1, request_id.clone())), Response::None);
+		});
+	}
+}
