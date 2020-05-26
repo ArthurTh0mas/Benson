7bf048e04797219d8c8513eddf8a0a43e93c1d22
Annie Lai <68039932+ng8eke@users.noreply.github.com>
2020-05-26 20:15:07 +0800

    offline phragmén multithreaded

--- crml/staking/src/migration.rs
+++ crml/staking/src/migration.rs
@@ -1,148 +1,100 @@
-// Copyright 2017-2020 Parity Technologies (UK) Ltd. and Annie Lai Investments Ltd.
-// This file is part of Substrate.
+use super::*;
+use frame_support::Twox64Concat;
 
-// Substrate is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+/// Update storages to current version
+///
+/// In old version the staking module has several issue about handling session delay, the
+/// current era was always considered the active one.
+///
+/// After the migration the current era will still be considered the active one for the era of
+/// the upgrade. And the delay issue will be fixed when planning the next era.
+// * create:
+//   * ActiveEra
+//   * ErasStakers
+//   * ErasStakersClipped
+//   * ErasValidatorPrefs
+//   * ErasTotalStake
+//   * ErasStartSessionIndex
+// * removal of:
+//   * Stakers
+//   * SlotStake
+//   * CurrentElected
+//   * CurrentEraStart
+//   * CurrentEraStartSessionIndex
+pub fn upgrade_v1_to_v2<T: Trait>() {
+	/// Deprecated storages used for migration only.
+	mod deprecated {
+		use crate::{BalanceOf, Exposure, SessionIndex, Trait};
+		use frame_support::{decl_module, decl_storage};
+		use sp_std::prelude::*;
 
-// Substrate is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+		decl_module! { pub struct Module<T: Trait> for enum Call where origin: T::Origin {} }
 
-// You should have received a copy of the GNU General Public License
-// along with Substrate.  If not, see <http://www.gnu.org/licenses/>.
+		decl_storage! {
+			pub trait Store for Module<T: Trait> as Staking {
+				pub SlotStake: BalanceOf<T>;
 
-//! Storage migrations for srml-staking.
+				/// The currently elected validator set keyed by stash account ID.
+				pub CurrentElected: Vec<T::AccountId>;
 
-/// Indicator of a version of a storage layout.
-pub type VersionNumber = u32;
+				/// The start of the current era.
+				pub CurrentEraStart: u64; // 'MomentOf<T>' in v38
 
-// the current expected version of the storage
-pub const CURRENT_VERSION: VersionNumber = 2;
+				/// The session index at which the current era started.
+				pub CurrentEraStartSessionIndex: SessionIndex;
 
-/// The inner logic of migrations.
-#[cfg(any(test, feature = "migrate"))]
-pub mod inner {
-	use super::{VersionNumber, CURRENT_VERSION};
-	use crate::{Module, Store, Trait};
-	use codec::{Decode, Encode};
-	use frame_support::{StorageLinkedMap, StoragePrefixedMap, StorageValue};
-	use sp_std::vec::Vec;
-
-	// the minimum supported version of the migration logic.
-	const MIN_SUPPORTED_VERSION: VersionNumber = 0;
-
-	// migrate storage from v0 to v1.
-	//
-	// this upgrades the `Nominators` linked_map value type from `Vec<T::AccountId>` to
-	// `Option<Nominations<T::AccountId>>`
-	pub fn to_v1<T: Trait>(version: &mut VersionNumber) {
-		if *version != 0 {
-			return
-		}
-		*version += 1;
-
-		let now = <Module<T>>::current_era();
-		let res = <Module<T> as Store>::Nominators::translate::<T::AccountId, Vec<T::AccountId>, _, _>(
-			|key| key,
-			|targets| crate::Nominations {
-				targets,
-				submitted_in: now,
-				suppressed: false,
-			},
-		);
-
-		if let Err(e) = res {
-			frame_support::print("Encountered error in migration of Staking::Nominators map.");
-			if e.is_none() {
-				frame_support::print("Staking::Nominators map reinitialized");
-			}
-		}
-
-		frame_support::print("Finished migrating Staking storage to v1.");
-	}
-
-	// migrate storage from v1 to v2: adds another field to the `SlashingSpans`
-	// struct.
-	pub fn to_v2<T: Trait>(version: &mut VersionNumber) {
-		use crate::{slashing::SpanIndex, EraIndex};
-		#[derive(Decode)]
-		struct V1SlashingSpans {
-			span_index: SpanIndex,
-			last_start: EraIndex,
-			prior: Vec<EraIndex>,
-		}
-
-		#[derive(Encode)]
-		struct V2SlashingSpans {
-			span_index: SpanIndex,
-			last_start: EraIndex,
-			last_nonzero_slash: EraIndex,
-			prior: Vec<EraIndex>,
-		}
-
-		if *version != 1 {
-			return
-		}
-		*version += 1;
-
-		let prefix = <Module<T> as Store>::SlashingSpans::final_prefix();
-		let mut current_key = prefix.to_vec();
-		loop {
-			let maybe_next_key = sp_io::storage::next_key(&current_key[..]).filter(|v| v.starts_with(&prefix[..]));
-
-			match maybe_next_key {
-				Some(next_key) => {
-					let maybe_spans =
-						sp_io::storage::get(&next_key[..]).and_then(|v| V1SlashingSpans::decode(&mut &v[..]).ok());
-					if let Some(spans) = maybe_spans {
-						let new_val = V2SlashingSpans {
-							span_index: spans.span_index,
-							last_start: spans.last_start,
-							last_nonzero_slash: spans.last_start,
-							prior: spans.prior,
-						}
-						.encode();
-
-						sp_io::storage::set(&next_key[..], &new_val[..]);
-					}
-					current_key = next_key;
-				}
-				None => break,
+				/// Nominators for a particular account that is in action right now. You can't iterate
+				/// through validators here, but you can find them in the Session module.
+				///
+				/// This is keyed by the stash account.
+				pub Stakers: map hasher(twox_64_concat) T::AccountId => Exposure<T::AccountId, BalanceOf<T>>;
 			}
 		}
 	}
 
-	pub(super) fn perform_migrations<T: Trait>() {
-		<Module<T> as Store>::StorageVersion::mutate(|version| {
-			if *version < MIN_SUPPORTED_VERSION {
-				frame_support::print(
-					"Cannot migrate staking storage because version is less than\
-					minimum.",
-				);
-				frame_support::print(*version);
-				return
-			}
-
-			if *version == CURRENT_VERSION {
-				return
-			}
-
-			to_v1::<T>(version);
-			to_v2::<T>(version);
-		});
+	for (_stash, controller) in Bonded::<T>::iter() {
+		// Ledger is twox_64_concat in v38, transition to blake256
+		Ledger::<T>::migrate_key::<Twox64Concat, T::AccountId>(controller);
 	}
-}
 
-#[cfg(not(any(test, feature = "migrate")))]
-mod inner {
-	pub(super) fn perform_migrations<T>() {}
-}
+	let current_era_start_index = deprecated::CurrentEraStartSessionIndex::get();
+	let current_era = <Module<T> as Store>::CurrentEra::get().unwrap_or(0);
+	let current_era_start = deprecated::CurrentEraStart::get();
+	<Module<T> as Store>::ErasStartSessionIndex::insert(current_era, current_era_start_index);
+	<Module<T> as Store>::ActiveEra::put(ActiveEraInfo {
+		index: current_era,
+		start: Some(current_era_start),
+	});
 
-/// Perform all necessary storage migrations to get storage into the expected stsate for current
-/// logic. No-op if fully upgraded.
-pub(crate) fn perform_migrations<T: crate::Trait>() {
-	inner::perform_migrations::<T>();
+	let current_elected = deprecated::CurrentElected::<T>::get();
+	let mut current_total_stake = <BalanceOf<T>>::zero();
+	for validator in &current_elected {
+		let exposure = deprecated::Stakers::<T>::get(validator);
+		current_total_stake += exposure.total;
+		<Module<T> as Store>::ErasStakers::insert(current_era, validator, &exposure);
+
+		let mut exposure_clipped = exposure;
+		let clipped_max_len = T::MaxNominatorRewardedPerValidator::get() as usize;
+		if exposure_clipped.others.len() > clipped_max_len {
+			exposure_clipped
+				.others
+				.sort_unstable_by(|a, b| a.value.cmp(&b.value).reverse());
+			exposure_clipped.others.truncate(clipped_max_len);
+		}
+		<Module<T> as Store>::ErasStakersClipped::insert(current_era, validator, exposure_clipped);
+
+		let pref = <Module<T> as Store>::Validators::get(validator);
+		<Module<T> as Store>::ErasValidatorPrefs::insert(current_era, validator, pref);
+	}
+	<Module<T> as Store>::ErasTotalStake::insert(current_era, current_total_stake);
+
+	// Kill old storages
+	deprecated::Stakers::<T>::remove_all();
+	deprecated::SlotStake::<T>::kill();
+	deprecated::CurrentElected::<T>::kill();
+	deprecated::CurrentEraStart::kill();
+	deprecated::CurrentEraStartSessionIndex::kill();
+
+	// Release
+	StorageVersion::put(Releases::V2 as u32);
 }
